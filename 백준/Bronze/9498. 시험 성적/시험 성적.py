a23e8c39a16606052cf8547871da5b5df861adbd
kiwanPrac <kiwan.prac@gmail.com>
2024-08-11 01:43:47 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,19 @@
+A = input()
+
+X = int(A)
+
+
+if 90 <= X  : 
+    print("A")
+
+elif 80 <= X  : 
+    print("B")
+    
+elif 70 <= X  : 
+    print("C")
+
+elif 60 <= X  : 
+    print("D")
+
+else : 
+    print("F")
