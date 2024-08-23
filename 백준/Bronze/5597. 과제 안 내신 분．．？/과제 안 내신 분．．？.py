dc550deaea32925ba2d762eea333f274d7e323b1
kiwanPrac <kiwan.prac@gmail.com>
2024-08-24 02:34:12 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,14 @@
+student = []
+
+#Add student amount 1 to 30
+for i in range(1,31):
+    student.append(i)
+
+#remove 28 time, submit student's list
+for j in range(28):
+    n = int(input())
+    student.remove(n)
+
+#print list
+for j in range(2):
+    print(student[j])
